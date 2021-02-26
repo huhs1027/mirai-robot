1ecdf622baee2fc68229c824b9f842bdfac2629c
hhs <hhs@servyou.com.cn>
2021-02-26 16:20:58 +0800

    指令关闭机器人复读功能

--- src/main/java/com/hhs/robot/mirairobot/app/handler/group/RepeaterCommend.java
+++ src/main/java/com/hhs/robot/mirairobot/app/handler/group/RepeaterCommend.java
@@ -1,12 +1,20 @@
 package com.hhs.robot.mirairobot.app.handler.group;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.hhs.robot.mirairobot.app.handler.GroupEventCommend;
 import com.hhs.robot.mirairobot.core.annon.Commend;
 import com.hhs.robot.mirairobot.core.component.MessageVO;
 import com.hhs.robot.mirairobot.core.utils.RandomUtils;
+import com.hhs.robot.mirairobot.dao.entity.ShutUpEntity;
+import com.hhs.robot.mirairobot.dao.mapper.ShutUpMapper;
 import net.mamoe.mirai.event.events.GroupMessageEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
 
 /**
  * @author hhs
@@ -15,9 +23,12 @@ import java.util.ArrayList;
 @Commend(group = { 467236828 }, commend = false)
 public class RepeaterCommend implements GroupEventCommend {
 
+    @Autowired
+    private ShutUpMapper shutUpMapper;
+
     @Override
     public MessageVO handlerGroupMessage(GroupMessageEvent groupMessageEvent) {
-        // 随机 1/3条重复1条
+        // 随机 10%
         if (RandomUtils.win(1)) {
             return new MessageVO().add(groupMessageEvent.getMessage());
         }
@@ -33,7 +44,14 @@ public class RepeaterCommend implements GroupEventCommend {
 
         boolean robotContains = groupMessageEvent.getMessage().contentToString().contains("at:2854200117");
 
-        return !filterList.contains(groupMessageEvent.getSender().getId()) && !robotContains;
+        // 指令关闭复读
+        Supplier<List<ShutUpEntity>> supplier = () -> {
+            QueryWrapper<ShutUpEntity> entityQueryWrapper = new QueryWrapper<>();
+            entityQueryWrapper.gt("expire", LocalDateTime.now());
+            return shutUpMapper.selectList(entityQueryWrapper);
+        };
+
+        return !filterList.contains(groupMessageEvent.getSender().getId()) && !robotContains && CollectionUtils.isEmpty(supplier.get());
     }
 
     @Override
